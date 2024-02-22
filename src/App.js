78f71f59f4677eac86141325053191b7f376dacb
Danielle Lindblom <danielle.lindblom@gmail.com>
2024-02-22 08:28:30 -0700

    button type passed via props based on list type

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import dataset from "./data.js"
 function App() {
   
   const newDataset = dataset.map(
-    item => ({...item, listType: "resultsList" })
+    item => ({...item, listType: "playList" })
   )
 
   
--- src/components/Track/Track.js
+++ src/components/Track/Track.js
@@ -2,20 +2,13 @@ import React, { useState } from "react"
 import "./Track.css"
 
 export default function Track(props) {
-    const [buttonValue, setButtonValue] = useState(true)
-
-    function ChangeButton() {
-        setButtonValue(prevButtonValue => !prevButtonValue)
-    }
-    
-    
+   
     return (
         <div className="track">
             <p className="title">{props.title}</p>
             <p className="details">{props.artist}</p>
             <p className="details">{props.album}</p>
-            <p>{props.listType}</p>
-            <button onClick={ChangeButton} type="button" className="button">{buttonValue ? "+" : "-"}</button>
+            <button type="button" className="button">{props.listType === "resultsList" ? "+" : "-"}</button>
         </div>
     )
 }
